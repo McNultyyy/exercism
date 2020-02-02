148dbac2cb29599904e08250a30894f91ba07982
William McNulty <william.mcnulty@live.co.uk>
2020-02-02 11:42:37 +0000

    added first few examples

---
+++ raindrops/Raindrops.fs
@@ -0,0 +1,24 @@
+﻿module Raindrops
+
+let positiveNumbers =
+    Seq.unfold (fun n -> Some(n , n + 1)) 0
+
+let isFactorOf a b =
+    b % a = 0
+
+
+let factorsOf n =
+    [1..n-1]
+    |> isFactorOf n 
+
+let dropMap = 
+    Map.empty.
+        Add(3, "Pling").
+        Add(5, "Plong").
+        Add(7, "Plang")
+
+let convert (number: int): string =
+    dropMap
+    |> Map.toSeq
+    |> Seq.map fst
+    |> factors
