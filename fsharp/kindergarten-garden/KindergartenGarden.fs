6004c71b28e0d8460e3091ace097ccebba96760b
William McNulty <william.mcnulty@live.co.uk>
2020-11-15 20:37:03 +0000

    fixed compiler errors

--- fsharp/kindergarten-garden/KindergartenGarden.fs
+++ fsharp/kindergarten-garden/KindergartenGarden.fs
@@ -2,4 +2,17 @@
 
 // TODO: define the Plant type
 
+type Plant =
+    | Radishes
+    | Clover
+    | Grass
+    | Violets
+
+type Child =
+    | Alice | Bob | Charlie | David
+    | Eve | Fred | Ginny | Harriet
+    | Ilena | Joseph | Kincaid | Larry
+
+
+
 let plants diagram student = failwith "You need to implement this function."
