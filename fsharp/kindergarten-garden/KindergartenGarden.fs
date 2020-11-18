2f3d6beb82debbf4439b519c0a7f229c25897c88
William McNulty <william.mcnulty@live.co.uk>
2020-11-18 20:11:47 +0000

    Added implementation for Kindergarden question

--- fsharp/kindergarten-garden/KindergartenGarden.fs
+++ fsharp/kindergarten-garden/KindergartenGarden.fs
@@ -8,11 +8,37 @@ type Plant =
     | Grass
     | Violets
 
-type Child =
-    | Alice | Bob | Charlie | David
-    | Eve | Fred | Ginny | Harriet
-    | Ilena | Joseph | Kincaid | Larry
+let characterToPlant (character: string) =
+    match character with
+    | "R" -> Radishes
+    | "C" -> Clover
+    | "G" -> Grass
+    | "V" -> Violets
+
+let students =
+    [ "Alice"
+      "Bob"
+      "Charlie"
+      "David"
+      "Eve"
+      "Fred"
+      "Ginny"
+      "Harriet"
+      "Ilena"
+      "Joseph"
+      "Kincaid"
+      "Larry" ]
 
 
+let plants (diagram: string) (student: string): (Plant list) =
 
-let plants diagram student = failwith "You need to implement this function."
+    let studentIndex =
+        List.findIndex (fun x -> x = student) students
+
+    let answer =
+        diagram.Split '\n'
+        |> Seq.map (fun x -> x.Substring(studentIndex * 2, 2))
+        |> Seq.reduce (+)
+        |> Seq.map (fun c -> c.ToString() |> characterToPlant)
+
+    answer |> List.ofSeq
