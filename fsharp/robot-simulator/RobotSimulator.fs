5180d89bc1afca742d614ef0cafbb7842a49a39b
William McNulty <william.mcnulty@live.co.uk>
2020-11-19 23:09:41 +0000

    Completed question for RobotSimulator

--- fsharp/robot-simulator/RobotSimulator.fs
+++ fsharp/robot-simulator/RobotSimulator.fs
@@ -1,9 +1,61 @@
 ﻿module RobotSimulator
 
-type Direction = North | East | South | West
+type Direction =
+    | North
+    | East
+    | South
+    | West
+
 type Position = int * int
-type Robot = { direction: Direction; position: Position }
 
-let create direction position = failwith "You need to implement this function."
+type Robot =
+    { direction: Direction
+      position: Position }
 
-let move instructions robot = failwith "You need to implement this function."
+type Action =
+    | RotateLeft
+    | RotateRight
+    | Accelerate
+
+
+let create direction position =
+    { direction = direction
+      position = position }
+
+
+
+
+let turnLeft robot =
+    let newDirection =
+        match robot.direction with
+        | North -> West
+        | East -> North
+        | South -> East
+        | West -> South
+
+    { robot with direction = newDirection }
+
+let turnRight = turnLeft >> turnLeft >> turnLeft
+
+let accelerate ({ direction = direction
+                  position = (x, y) } as robot) =
+
+    let newPosition =
+        match direction with
+        | North -> (x, y + 1)
+        | East -> (x + 1, y)
+        | South -> (x, y - 1)
+        | West -> (x - 1, y)
+
+    { robot with position = newPosition }
+
+let toAction instruction =
+    match instruction with
+    | 'L' -> turnLeft
+    | 'R' -> turnRight
+    | 'A' -> accelerate
+
+let move instructions robot =
+    instructions
+    |> Seq.map toAction
+    |> Seq.fold (|>) robot
